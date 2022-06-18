a474320b0fc3ba58ff43f112a4a18b5582c786ac
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-06-19 04:44:15 +0530

    Update __init__.py

--- Mariamusic/database/__init__.py
+++ Mariamusic/database/__init__.py
@@ -1,5 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-from m8n.config import MONGO_DB_URI
+from Mariamusic.config import MONGO_DB_URI
 
 
 MONGODB_CLI = MongoClient(MONGO_DB_URI)
