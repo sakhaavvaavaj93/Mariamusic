6538415140b05f5f77127c7edff616b88ca79831
സഖാവ് വാവ <73136964+sakhaavvaavaj93@users.noreply.github.com>
2022-06-20 21:36:42 +0530

    Update play.py

--- Mariamusic/modules/play.py
+++ Mariamusic/modules/play.py
@@ -397,7 +397,7 @@ async def play(_, message: Message):
             )
             return
         requested_by = message.from_user.first_name
-            await generate_cover(requested_by, title, views, duration, thumbnail)
+        await generate_cover(requested_by, title, views, duration, thumbnail)
 
         def my_hook(d):
             if d["status"] == "downloading":
